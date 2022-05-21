dbb985590d88d6c7b980a844103d975931cd03e2
ClemenceLee <100284963+ClemenceLee@users.noreply.github.com>
2022-05-21 23:28:16 +0800

    Add files via upload

---
+++ 小贴士/xts.js
@@ -0,0 +1,31 @@
+// 获取所有的box
+let boxList = document.querySelectorAll('.box');
+
+// 监听滚动
+window.addEventListener('scroll', scrollLoad);
+
+// 滚动加载函数
+function scrollLoad(){
+
+    // 1.定义一个目标值
+    let targetValue = window.innerHeight * 0.8;
+    // 屏幕高度的80%
+
+    // 2.获取每一个box距离最顶部的值
+    boxList.forEach(box=>{
+        let boxTop = box.getBoundingClientRect().top;
+        // 获取每个盒子距离顶部的值
+
+        if(boxTop <= targetValue){
+            // 滑到小于等于的位置
+            // 各个方位预备备！出现！
+            box.classList.add('show-center');
+            // 添加元素类名
+        }
+        else{
+            box.classList.remove('show-center');
+        }
+    })
+
+
+}
